ac382d3be6147cce62b4c7137b89527fc8a6a7a1
aamir7shahab <aamir.shahab007@gmail.com>
2023-02-20 17:23:51 +0530

    Testing webhook

---
+++ backup_lambda.py
@@ -0,0 +1,66 @@
+
+import logging
+import json
+import hmac
+import hashlib
+import re
+import datetime
+
+import boto3
+import base64
+from botocore.exceptions import ClientError
+
+from urllib.parse import unquote
+from pull_secret import get_secret
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+events = boto3.client('events')
+
+def process(content):
+    entries = [
+            {
+                'Source': "lambda.github.webhook",
+                "DetailType": "GitHub Webhook",
+                "Detail": json.dumps(content),
+
+            }
+        ]
+    event_response = events.put_events(Entries=entries)
+
+
+def lambda_handler(event, context):
+    logger.info("Lambda execution starting up...")
+    
+    if not authenticate(event):
+        logger.error('Unauthorized attempt')
+        return {
+            'statusCode': 401,
+            'body': json.dumps('Unauthorized')
+        }
+
+    logger.info('Request successfully authorized')
+    print(event)
+    print(json.loads(event['body']))
+    process(json.loads(event['body']))
+
+    return {
+        'statusCode': 204
+    }
+
+
+def authenticate(event):
+    incoming_signature = re.sub(r'^sha1=', '', event['headers']['X-Hub-Signature'])
+    incoming_payload = json.dumps(json.loads(unquote(re.sub(r'^payload=', '', event['body']))), separators=(',', ':'))
+    
+    calculated_signature = calculate_signature(json.loads(get_secret())["webhash"], incoming_payload.encode('utf-8'))
+    
+    return hmac.compare_digest(incoming_signature, calculated_signature)
+
+def calculate_signature(github_signature, githhub_payload):
+    signature_bytes = github_signature.encode("utf-8")
+    digest = hmac.new(key=signature_bytes, msg=githhub_payload, digestmod=hashlib.sha1)
+    signature = digest.hexdigest()
+    return signature
+
